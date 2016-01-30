bb7078a3dd0ed38b2006d59e12c809a8feaaa529
Weslo <wesley.rockholz@gmail.com>
2016-01-30 18:21:48 -0500

    get the character working

---
+++ Assets/Scripts/Utilities/PrefabSpawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Spawns a prefab to its position and then destroys itself.
+/// </summary>
+public class PrefabSpawner : MonoBehaviour {
+
+	public GameObject prefab;
+
+	void Awake () {
+		GameObject instantiated = Instantiate (prefab) as GameObject;
+		instantiated.transform.SetParent (transform.parent);
+		instantiated.transform.localPosition = transform.localPosition;
+		instantiated.transform.localScale = transform.localScale;
+		instantiated.transform.localRotation = transform.localRotation;
+		Destroy (gameObject);
+	}
+}

3d77c8494840b21cfb9a43541befc147e7f29696
Weslo <wesley.rockholz@gmail.com>
2016-01-30 16:13:49 -0500

    set up join screen stuff

---
+++ Assets/Scripts/StateManagement/GameStates/JoinGameScreen.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+// Controls the main menu screen.
+public class JoinGameScreen : IGameState {
+
+	// The currently entered room code.
+	public string RoomCode { get; set; }
+
+	// Called when the join game button is clicked.
+	public void OnClickJoinGameButton () {
+		Debug.LogWarning ("Join game not hooked to server. Moving to test this functionality client-side.");
+	}
+}
---
+++ Assets/Scripts/Utilities/UppercaseInputField.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+// Forces an input field to convert all text to uppercase.
+[ExecuteInEditMode, RequireComponent (typeof (InputField))]
+public class UppercaseInputField : MonoBehaviour {
+
+	// Update this component.
+	void Update () {
+		InputField input = GetComponent<InputField> ();
+		input.text = input.text.ToUpper ();
+	}
+}

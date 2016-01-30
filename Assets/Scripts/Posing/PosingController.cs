5a420f76762c88aecd552cf2a173c9496edcd8f3
Weslo <wesley.rockholz@gmail.com>
2016-01-30 15:07:40 -0500

    get animating poses working

--- Assets/Scripts/Posing/PosingController.cs
+++ Assets/Scripts/Posing/PosingController.cs
@@ -1,20 +1,81 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 // Relationship between a handle and a position.
 public class HandlePositionPair {
-	long handleID;
-	Vector2 position;
+	public long handleID;
+	public Vector2 position;
+}
+
+// Set of handle-position-pairs.
+public class HandlePositionPairSet {
+	public List<HandlePositionPair> handlePositionPairs = new List<HandlePositionPair> ();
+
+	// Get handle-position pair by the handle ID.
+	public HandlePositionPair GetPairByHandleID (long handleID) {
+		for (int i = 0; i < handlePositionPairs.Count; i++) {
+			HandlePositionPair pair = handlePositionPairs [i];
+			if (pair.handleID == handleID) {
+				return pair;
+			}
+		}
+		return null;
+	}
 }
 
 // Manages a single posed rig.
 public class PosingController : MonoBehaviour {
 
+	// Time between each keyframe.
+	[SerializeField] private float timeBetweenKeyframes = 2f;
+
+	// Animation time tracker.
+	private float t = 0;
+
+	// Current animation frame.
+	private int currentFrame = 0;
+
 	// References to all the handles in this rig.
-	HandleController[] handles;
+	private HandleController[] handles;
+
+	// Set of handle-position pairs to play across keyframes.
+	private HandlePositionPairSet[] playingKeyframes;
+
+	// If set to true, this posing controller is just playing an animation and can't be controlled.
+	private bool playing = false;
 
 	// Initialize this component.
 	void Start () {
 		handles = GetComponentsInChildren<HandleController> ();
+	}
+
+	// Update this component.
+	void Update () {
+		if (playing) {
+
+			// Update time.
+			t += Time.deltaTime;
+			if (t >= timeBetweenKeyframes) {
+				t -= timeBetweenKeyframes;
+				currentFrame = (currentFrame + 1) % playingKeyframes.Length;
+			}
+
+			// Get the next frame.
+			int nextFrame = (currentFrame + 1) % playingKeyframes.Length;
+
+			// Interpolate the positions of each of the handles.
+			HandlePositionPairSet currentKey = playingKeyframes [currentFrame];
+			HandlePositionPairSet nextKey = playingKeyframes [nextFrame];
+			for (int i = 0; i < handles.Length; i++) {
+				HandleController handle = handles [i];
+				HandlePositionPair current = currentKey.GetPairByHandleID (handle.id);
+				HandlePositionPair next = nextKey.GetPairByHandleID (handle.id);
+				handle.transform.position = new Vector2 (
+					Mathf.Lerp (current.position.x, next.position.x, t / timeBetweenKeyframes),
+					Mathf.Lerp (current.position.y, next.position.y, t / timeBetweenKeyframes)
+				);
+			}
+		}
 	}
 
 	// Sets whether or not all the handles are visible.
@@ -22,5 +83,42 @@ public class PosingController : MonoBehaviour {
 		for (int i = 0; i < handles.Length; i++) {
 			handles [i].SetVisible (visible);
 		}
+	}
+
+	// Collect the set of handle position pairs.
+	public HandlePositionPairSet CollectHandlePositions () {
+		HandlePositionPairSet pairs = new HandlePositionPairSet ();
+		for (int i = 0; i < handles.Length; i++) {
+			HandleController handle = handles [i];
+			pairs.handlePositionPairs.Add (
+				new HandlePositionPair {
+					handleID = handle.id,
+					position = handle.transform.position,
+				}
+			);
+		}
+		return pairs;
+	}
+
+	// Play an animation given a bunch of handle-position pairs and frames.
+	public void PlayAnimation (HandlePositionPairSet[] pairs) {
+
+		// Can't see handles while playing.
+		SetAllHandlesVisible (false);
+
+		// Copy pairs and start playing.
+		playingKeyframes = pairs;
+		playing = true;
+	}
+
+	// Get a handle controller via its ID.
+	public HandleController GetHandleByID (long handleID) {
+		for (int i = 0; i < handles.Length; i++) {
+			HandleController handle = handles [i];
+			if (handle.id == handleID) {
+				return handle;
+			}
+		}
+		return null;
 	}
 }
--- Assets/Scripts/StateManagement/GameStates/PosingScreen.cs
+++ Assets/Scripts/StateManagement/GameStates/PosingScreen.cs
@@ -3,5 +3,30 @@
 // Manages the screen where users pose their characters
 public class PosingScreen : IGameState {
 
+	public int numFrames = 3;
+
+	// Reference to the rig in the scene.
+	private PosingController rig;
+
 	private int currentFrame = 0;
+
+	// The stored keyframes for the pending animation.
+	private HandlePositionPairSet[] keyFrames;
+
+	// Initialize this game state.
+	public override void OnInitializeState () {
+		base.OnInitializeState ();
+		rig = GetComponentInChildren<PosingController> ();
+		keyFrames = new HandlePositionPairSet [numFrames];
+	}
+
+	// Save the pose to the current key frame and increment.
+	public void SavePose () {
+		keyFrames [currentFrame] = rig.CollectHandlePositions ();
+		currentFrame++;
+
+		if (currentFrame >= numFrames) {
+			rig.PlayAnimation (keyFrames);
+		}
+	}
 }

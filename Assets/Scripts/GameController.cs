4b361c0892bb671a1ca830bcd178cf7b70babb5d
Weslo <wesley.rockholz@gmail.com>
2016-01-30 21:33:25 -0500

    game controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+// A bird.
+[Serializable]
+public class Bird {
+	public string name;
+	public Sprite headSprite;
+	public PosingController rig;
+}
+
+// Manages the GameController component.
+public class GameController : MonoBehaviour {
+
+	// Array of birds.
+	[SerializeField] private Bird[] birds;
+
+	// Singleton instance of the GameController component.
+	private static GameController singleton;
+
+	// Initialize this component.
+	void Awake () {
+		singleton = this;
+	}
+
+	// Get a bird by its index.
+	public Bird GetBird (int index) {
+		return birds [index];
+	}
+}

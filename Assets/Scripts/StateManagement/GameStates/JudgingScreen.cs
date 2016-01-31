ad26930a2e75bf78ba6f4998acf6ad1a4a366fd8
Weslo <wesley.rockholz@gmail.com>
2016-01-31 12:04:56 -0500

    get camera drag working for mouse in the judging screen

--- Assets/Scripts/StateManagement/GameStates/JudgingScreen.cs
+++ Assets/Scripts/StateManagement/GameStates/JudgingScreen.cs
@@ -5,15 +5,41 @@ using UnityEngine.UI;
 public class JudgingScreen : IGameState {
 
 	// The space between posed characters on the stage.
-	[SerializeField] private float characterSpacing = 3f;
+	[SerializeField] private float characterSpacing = 10f;
 
 	// Prefab for the button to select a mate.
 	[SerializeField] private Button selectMateButtonPrefab;
+
+	// Camera end bounds on right side.
+	private float cameraEndBound = 0;
 
 	// Initialize this game state.
 	public override void OnInitializeState () {
 		base.OnInitializeState ();
 		LoadRigs (new int[] { 0, 1, 2, 4 }, new HandlePositionPairSet[4][]);
+
+		// Ensure camera drag component.
+		CameraDrag drag = Camera.main.GetComponent<CameraDrag> ();
+		if (drag == null) {
+			drag = Camera.main.gameObject.AddComponent<CameraDrag> ();
+		}
+		drag.vertical = false;
+	}
+
+	// Update this game state.
+	public override void OnUpdateState () {
+		base.OnUpdateState ();
+
+		// Clamp camera to bounds.
+		float cameraX = Camera.main.transform.position.x;
+		cameraX = Mathf.Clamp (cameraX, 0, cameraEndBound);
+		Camera.main.transform.position = new Vector3 (cameraX, Camera.main.transform.position.y, Camera.main.transform.position.z);
+	}
+
+	// Clean up this game state before exiting.
+	public override void OnExitState () {
+		base.OnExitState ();
+		Destroy (Camera.main.GetComponent<CameraDrag> ());
 	}
 
 	// Load the rigs and their animations on to this screen.
@@ -38,6 +64,7 @@ public class JudgingScreen : IGameState {
 			selectButton.transform.position = new Vector3 (i * characterSpacing, selectMateButtonPrefab.transform.position.y, 0);
 			selectButton.onClick.AddListener (() => OnSelectMate (characterIndex));
 		}
+		cameraEndBound = characterSpacing * (characterIndices.Length - 1);
 	}
 
 	// Called when the button to select the mate at a given index is pressed.

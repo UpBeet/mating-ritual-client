d2d57579308698179ba108c0b0edbad479b1ac7f
Weslo <wesley.rockholz@gmail.com>
2016-01-31 12:21:11 -0500

    judging animations

--- Assets/Scripts/StateManagement/GameStates/JudgingScreen.cs
+++ Assets/Scripts/StateManagement/GameStates/JudgingScreen.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 // Controls the judging screen.
 public class JudgingScreen : IGameState {
@@ -10,8 +11,14 @@ public class JudgingScreen : IGameState {
 	// Prefab for the button to select a mate.
 	[SerializeField] private Button selectMateButtonPrefab;
 
+	// Animation that gets instantiated when the winner is shown.
+	[SerializeField] private GameObject winnerAnimation;
+
 	// Camera end bounds on right side.
 	private float cameraEndBound = 0;
+
+	// Dictionary of rigs by character index.
+	private Dictionary<int, PosingController> rigs;
 
 	// Initialize this game state.
 	public override void OnInitializeState () {
@@ -46,6 +53,7 @@ public class JudgingScreen : IGameState {
 	public void LoadRigs (int[] characterIndices, HandlePositionPairSet[][] animations) {
 		Transform container = transform.Find ("Stage");
 		container.DestroyAllChildren ();
+		rigs = new Dictionary<int, PosingController> ();
 		for (int i = 0; i < characterIndices.Length; i++) {
 			int characterIndex = characterIndices [i];
 
@@ -56,6 +64,7 @@ public class JudgingScreen : IGameState {
 			rig.transform.localScale = Vector3.one;
 			rig.transform.position = new Vector3 (i * characterSpacing, 0, 0);
 			rig.PlayAnimation (animations [i]);
+			rigs.Add (characterIndex, rig);
 
 			// Display the select mate button.
 			Button selectButton = Instantiate (selectMateButtonPrefab);
@@ -70,5 +79,21 @@ public class JudgingScreen : IGameState {
 	// Called when the button to select the mate at a given index is pressed.
 	private void OnSelectMate (int index) {
 		Debug.LogWarning ("Select mate at character index " + index + ".");
+		ShowWinner (index);
+	}
+
+	// Show the winner at the specified index.
+	private void ShowWinner (int index) {
+		Vector2 winnerPos = rigs [index].transform.position;
+
+		// Drag to winner.
+		CameraDrag drag = Camera.main.GetComponent<CameraDrag> ();
+		drag.horizontal = false;
+		drag.Focus (winnerPos);
+
+		// Show animation.
+		GameObject anim = Instantiate (winnerAnimation);
+		anim.transform.localScale = Vector3.one;
+		anim.transform.position = winnerPos;
 	}
 }

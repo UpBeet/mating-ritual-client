e3d20154557871f70eb4aa15b53939646e59411a
Weslo <wesley.rockholz@gmail.com>
2016-01-31 10:07:03 -0500

    start setting up the judging screen

---
+++ Assets/Scripts/StateManagement/GameStates/JudgingScreen.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Controls the judging screen.
+public class JudgingScreen : IGameState {
+
+	// The space between posed characters on the stage.
+	[SerializeField] private float characterSpacing = 3f;
+
+	// Initialize this game state.
+	public override void OnInitializeState () {
+		base.OnInitializeState ();
+		LoadRigs (new int[] { 0, 1, 2, 4 }, new HandlePositionPairSet[4][]);
+	}
+
+	// Load the rigs and their animations on to this screen.
+	public void LoadRigs (int[] characterIndices, HandlePositionPairSet[][] animations) {
+		Debug.LogWarning (characterIndices.Length);
+		for (int i = 0; i < characterIndices.Length; i++) {
+			Bird bird = GameController.GetBird (characterIndices [i]);
+			PosingController rig = Instantiate (bird.rig);
+			rig.transform.SetParent (transform);
+			rig.transform.localScale = Vector3.one;
+			rig.transform.position = new Vector3 (i * characterSpacing, 0, 0);
+			rig.PlayAnimation (animations [i]);
+		}
+	}
+}
